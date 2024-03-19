7e68bf02724d7cf500384f77c87e63b50f5806a4
jonathan442443 <159461170+jonathan442443@users.noreply.github.com>
2024-03-19 22:16:39 +0100

    Update functions.js

--- assets/JS/functions.js
+++ assets/JS/functions.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function(){
     sendForm() 
   }
   else{
-    if(window.location.pathname==="/addUser.html"){
+    if(window.location.pathname==="/Projet2/addUser.html"){
       btn.addEventListener("click",getForm) 
     }
   }
